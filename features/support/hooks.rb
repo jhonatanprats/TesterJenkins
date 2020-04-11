c4caaa7b1b327e35a0fb9773f90fe459dabfb351
jhonatanprats <jhonatanprates@hotmail.com>
2020-04-11 17:54:27 -0300

    correções para rodar o jenkins4

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,21 +1,46 @@
+# frozen_string_literal: true
+
+
+
 Before do
-    @login_page = LoginPage.new
-    @sidebar = SideBarView.new
-    @movie_page = MoviePage.new
-    page.current_window.resize_to(1440,900)
-    #page.driver.browser.manage.window.maximize
+
+  @login_page = LoginPage.new
+
+  @sidebar = SideBarView.new
+
+  @movie_page = MoviePage.new
+
+  page.current_window.resize_to(1440, 900)
+
+  # page.driver.browser.manage.window.maximize
+
 end
+
+
 
 Before ('@login') do
-    user = CONFIG["users"]["cat_manager"]
-    @login_page.go
-    @login_page.with(user["email"], user["pass"])
+
+  user = CONFIG['users']['cat_manager']
+
+  @login_page.go
+
+  @login_page.with(user['email'], user['pass'])
+
 end
 
+
+
 After do |scenario|
-    #if scenario.failed?
-        temp_shot = page.save_screenshot("log/temp_shot.png")
-        screenshot = Base64.encode64(File.open(temp_shot).read) #Convertendo a imagem para o formato base 64
-        embed(screenshot, "image/png", "Screenshot") #Anexa o screenshot no relatório
-    #end
+
+  # if scenario.failed?
+
+  temp_shot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+
+  screenshot = Base64.encode64(File.open(temp_shot).read) # Convertendo a imagem para o formato base 64
+
+  embed(screenshot, 'image/png', 'Screenshot') # Anexa o screenshot no relatório
+
+  # end
+
 end
+
